ee946b46bd7d914dd9af350ed270a6b5a4cd077a
kuduiso <ridhofauza799@gmail.com>
2022-08-13 12:40:03 -0400

    first commit

---
+++ scanning/portscan1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2
+
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.setdefaulttimeout(1)
+
+host = raw_input("[*] Enter The Host To Scan: ")
+
+def portscanner(port):
+	if sock.connect_ex((host, port)):
+		print "[!!] Port %d is closed" % (port)
+	else:
+		print "[+] Port %d is opened" % (port)
+
+for port in range(1, 100):
+	portscanner(port)
